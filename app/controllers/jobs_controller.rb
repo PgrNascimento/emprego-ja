1024e17aa94692655ac8a831be23fec59a3e6e49
QueroSerDev07 <qsd07@campuss-mini-2.home>
2017-02-08 22:00:04 -0200

    user creates job offer

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,4 +6,17 @@ class JobsController < ApplicationController
   def show
     @job = Job.find params[:id]
   end
+  def new
+    @job = Job.new
+  end
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to @job
+    end
+  end
+  private
+  def job_params
+    params.require(:job).permit(:title, :location, :category, :company, :description)
+  end
 end
