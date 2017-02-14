3ce6943be9b6e683123ddb82e63c1fe5b3ba2012
Allan e Luiza <alanoliveiran45@gmail.com>
2017-02-13 22:07:18 -0200

    user creates companies

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,20 @@
+class CompaniesController < ApplicationController
+  def new
+    @company = Company.new
+  end
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to company_path @company
+
+    end
+  end
+  def show
+    @company = Company.find params[:id]
+
+  end
+  private
+  def company_params
+    params.require(:company).permit(:name, :location, :mail, :phone)
+  end
+end
